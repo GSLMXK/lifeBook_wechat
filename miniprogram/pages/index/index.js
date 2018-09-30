b0272941936b23f8c17fc6b098179ba5b85d2a03
GSLMXK <532599835@qq.com>
2018-09-30 17:54:43 +0800

    xk001
    开始

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,46 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+  //检测用户微信信息是否与lifebook账号绑定
+  checkUserInfo :function(){
+    //是则跳转
+    //否则
+  }
+
+})
+
+
